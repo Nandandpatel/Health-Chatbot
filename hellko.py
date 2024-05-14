76e0c310565f97b6bce0cf586acbe88d16181e1b
Mohakg17 <manmps17@gmail.com>
2024-05-15 04:40:58 +0530

    fixed comments

--- hellko.py
+++ hellko.py
@@ -6,12 +6,12 @@ def message_probability(user_message, recognised_words, single_response=False, r
     message_certainty = 0
     has_required_words = True
 
-  #here we increse message probability
+  #here we increase message probability
     for word in user_message:
         if word in recognised_words:
             message_certainty += 1
 
-    # Calculates the percent of recognised words in a user message
+    # Calculates the percentage of recognised words in a user message
     percentage = float(message_certainty) / float(len(recognised_words))
 
     # Checks that the required words are in the string
